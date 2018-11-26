b51b3e1f11dbbdec1bcca2d8b68a9523e3c70e76
Indrit <indrit.zeqiri@gmail.com>
2018-11-26 16:16:51 +0100

    Create fasty_application.h

---
+++ src/fasty_application.h
@@ -0,0 +1,24 @@
+/*
+ * @Author: Indrit Zeqiri 
+ * @Date: 2018-11-26 15:46:57 
+ * @Last Modified by: Indrit Zeqiri
+ * @Last Modified time: 2018-11-26 15:47:31
+ */
+#ifndef FASTY_APP_H
+#define FASTY_APP_H
+
+
+#define APP_WEBSITE                 "Fasty Sample Website"
+#define APP_DOMAIN                  "your+url"
+#define APP_DESCRIPTION             "Sample Website with Fasty Web Application Engine"
+#define APP_VERSION                 "1.0"
+
+
+/* app user session */
+
+typedef struct {
+    int id;
+} ausession_t;
+
+
+#endif  /* FASTY_APP_H */
